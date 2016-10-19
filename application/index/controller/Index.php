5e915e2d0527692a15a6b53a3a491842976f8b36
bitterg <lovekugua@gmail.com>
2016-10-19 17:31:01 +0800

    add awasome icon

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,12 +1,24 @@
 <?php
 namespace app\index\controller;
-use think\Db;
-class Index
+use \think\Controller;
+use \think\View;
+use \think\Db;
+class Index extends \think\Controller
 {
-	private $tables = array("rank"=>"wx_rank");
+	private $tables = array("rank"=>"wx_rank","category"=>"wx_category");
     public function index()
     {
-    	$result = Db::query("select * from ".$this->tables['rank']);
-    	return json_encode($result);
+    	$sql = "select * from ".$this->tables['category'];
+    	$result = Db::query($sql);
+        // 模板变量赋值
+        $this->assign('category',$result);
+        $this->assign('email','thinkphp@qq.com');
+        // 或者批量赋值
+        $this->assign([
+            'name'  => 'ThinkPHP',
+            'email' => 'thinkphp@qq.com'
+        ]);
+        // 模板输出
+        return $this->fetch('index');
     }
 }
